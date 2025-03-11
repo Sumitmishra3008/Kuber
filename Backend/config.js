77577253b48bc435dde87de72e439c991222611a
Sumit Mishra <97983634+Sumitmishra3008@users.noreply.github.com>
2025-03-12 01:16:06 +0530

    mongo db

---
+++ Backend/config.js
@@ -0,0 +1,4 @@
+const JWT_secret = "sumit_secret";
+module.exports = {
+  JWT_secret: JWT_secret,
+};
--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 app.use(express.json());
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
 // const user = require("./routes/user");
 
 // app.use(user);
