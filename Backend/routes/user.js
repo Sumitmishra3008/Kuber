959e699f454df6322a8733ed02afd11b00a2695e
Sumit Mishra <97983634+Sumitmishra3008@users.noreply.github.com>
2025-03-13 12:05:05 +0530

    section of routes labelled for future references

--- Backend/routes/user.js
+++ Backend/routes/user.js
@@ -15,15 +15,17 @@ user.get("/", (req, res) => {
 });
 
 user.post("/register", async (req, res) => {
-  const reqpayload = usersignup.safeParse(req.body);
+  const reqpayload = usersignup.safeParse(req.body); //zod validation for inputs
   console.log(reqpayload);
   if (!reqpayload.success) {
     return res.status(411).json({ message: "invalid inputs" });
   }
+
   const { Username, password, firstname, lastname, email } = req.body;
   if (!Username || !password || !firstname || !lastname || !email) {
     return res.status(422).json({ error: "Please fill all the fields" });
   }
+
   try {
     const user = new User({
       Username,
@@ -32,16 +34,17 @@ user.post("/register", async (req, res) => {
       lastname,
       email,
     });
-    const existingUser = await User.findOne({ $or: [{ email }, { Username }] });
+
+    const existingUser = await User.findOne({ $or: [{ email }, { Username }] }); //finding if there is existing user with same email
     if (existingUser) {
       return res.status(400).json({ message: "User already exists" });
     }
 
-    const hashedPassword = await user.createHash(req.body.password);
+    const hashedPassword = await user.createHash(req.body.password); //hashing the password
     user.password = hashedPassword;
     await user.save();
     const userId = user._id;
-    const token = jwt.sign({ userId }, JWT_secret);
+    const token = jwt.sign({ userId }, JWT_secret); //generating token
     res
       .status(201)
       .json({ message: "User registered successfully", token: token });
@@ -56,16 +59,16 @@ user.post("/login", async (req, res) => {
     return res.status(422).json({ error: "Please fill all the fields" });
   }
   try {
-    const user = await User.findOne({ Username });
+    const user = await User.findOne({ Username }); //finding user with username
     if (!user) {
       return res.status(404).json({ error: "Invalid username" });
     }
-    const validPassword = await user.validatePassword(password);
+    const validPassword = await user.validatePassword(password); //validating password with hashing mechanism
     if (!validPassword) {
       return res.status(401).json({ error: "Invalid password" });
     }
     const userId = user._id;
-    const token = jwt.sign({ userId }, JWT_secret, { expiresIn: "24h" });
+    const token = jwt.sign({ userId }, JWT_secret, { expiresIn: "24h" }); //generating token
     res
       .status(200)
       .json({ message: "User logged in successfully", token: token });
